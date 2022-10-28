2160eb33fb180273f5a43c31d8f85f3d2cd71c75
yotam <yotam@dubiner.org>
2022-10-28 09:26:22 -0700

    more regex :pain:

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,64 +1,33 @@
 public class Testing {
     public static void main(String[] args) {
         String frac = "1_3/2 + 55_6/5 * 5 - 1/2 + 68/7 - 3_5/4";
-        frac = frac.replaceAll("(_[1-9/ ]*)|(-|\\+|\\*|\\/) (\\d+[^_]\\d)", " ")
+        String whole = frac.replaceAll("(_[1-9/ ]*)|(-|\\+|\\*|\\/) (\\d+[^_]\\d)", " ")
                 .strip().replaceAll(" +", " ");
-        System.out.println(frac);
-//        String frac1 = frac.substring(0, frac.indexOf(' '));
-//        String frac2 = frac.substring(frac.indexOf('+')+2);
-//        System.out.println(frac1 + frac2);
+        System.out.println(whole);
+        String temp = "";
 
-        /*
-        while (infix has characters left to process)
-{
-nextCharacter = next nonblank character of infix
-switch (nextCharacter)
-{
-case variable:
-valueStack.push(value of the variable nextCharacter)
-break
-case '^' :
-operatorStack.push(nextCharacter)
-break
-case '+' : case '-' : case '*' : case '/' :
-while (!operatorStack.isEmpty() and precedence of nextCharacter <= precedence of
-{
-// Execute operator at top of operatorStack
-topOperator = operatorStack.pop()
-operandTwo = valueStack.pop()
-operandOne = valueStack.pop()
-result = the result of the operation in topOperator and its operands valueStack.push(result)
-}
-operatorStack.push(nextCharacter)
-break
-case '(' :
-operatorStack.push(nextCharacter)
-break
-case ')' : // Stack is not empty if infix expression is valid
-topOperator = operatorStack.pop()
-while (topOperator != '(')
-{
-operandTwo = valueStack.pop()
-operandOne = valueStack.pop()
-result = the result of the operation in topOperator and its operands
-operandOne and operandTwo
-valueStack.push(result)
-topOperator = operatorStack.pop()
-}
-break
-default: break // Ignore unexpected characters
-}
-}
-while (!operatorStack.isEmpty())
-{
-topOperator = operatorStack.pop()
-operandTwo = valueStack.pop()
-operandOne = valueStack.pop()
-result = the result of the operation in topOperator and its operands
-operandOne and operandTwo
-valueStack.push(result)
-}
-return valueStack.peek()
-         */
+        int sum = 0;
+        // (\d+ (\*|\/) \d)
+        for (int i = 0; i < whole.length(); i++) {
+            char c = whole.charAt(i);
+            if(c == '*' || c == '/'){
+
+                whole = whole.replaceFirst("(\\d+ (\\*|\\/) \\d)", "");
+                System.out.println("temp is: " + temp);
+                sum += addString(temp, c);
+            }
+        }
+
+
+    }
+
+    public static int addString(String a, char op){
+        int n1 = Integer.parseInt(a.substring(0, a.indexOf(op)-1));
+        int n2 = Integer.parseInt(a.substring(a.indexOf(op)+2));
+        if(op == '+') return n1 + n2;
+        if(op == '-') return n1 - n2;
+        if(op == '*') return n1 * n2;
+        if(op == '/') return n1 / n2;
+        return -1;
     }
 }
